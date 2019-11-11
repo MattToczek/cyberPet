d8d5f5e4dd551cff138ae3f80c2ff5002657f07a
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-11 16:37:53 +0000

    adding HTML functionality and images to original console app

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ class Pet {
           setTimeout(() =>{
             resolve('done');
             this.thirst += this.thirstRate;
-            this.hunger += 1;
-            this.boredom += 1;
+            this.hunger += 5;
+            this.boredom += 2;
             console.log("counter done");
-            console.log(this.thirst);
-          }, 10);
+            thirstCount.value = 100 - this.thirst;
+            hungerCount.value = 100 - this.hunger;
+            boredomCount.value = 100 - this.boredom;
+          }, this.thirstRate*1000);
         });
       };
       
@@ -27,8 +29,25 @@ class Pet {
         async count(){                      //calls promise asyncronously every second until thirst hits 100
             while (this.thirst < 100) {
                 await cat.promFunc()
+                console.log(this.thirst);
+                if (this.thirst >= 100) {
+                    alert(`${this.name} died of thirst.`)    
+                } else if (this.hunger >= 100) {
+                    alert(`${this.name} died of hunger.`)    
+                }else if(this.boredom >= 100) {
+                    alert(`${this.name} died of boredom.`)    
+                }
+
+                // if (this.thirst >= 80) {
+                //     thirstCount.style.color = "red" ;  
+                // } else if (this.hunger >= 80) {
+                //     hungerCount.style.color = "red" 
+                // }else if(this.boredom >= 80) {
+                //     boredomCount.style.color = "red"   
+                // }
+
             }    
-            console.log(`${this.name} died of thirst.`)  
+             
     }
 
     revive() {                              //resets thirst to 0 and starts count() again
@@ -37,20 +56,37 @@ class Pet {
     }
 }
 
-let petSel = document.getElementById("petOpt");
+let petSel = document.getElementById("newPet");
 let img = document.getElementsByClassName("img")[0];
+let input = document.getElementsByClassName("input")[0];
+let screenDivs = document.getElementsByClassName("pet");
+let thirstCount = document.getElementById("thirstCount");
+let hungerCount = document.getElementById("hungerCount");
+let boredomCount = document.getElementById("boredomCount");
 
+let gameScreen = () => {
+    for (let i = 0; i < screenDivs.length; i++) {
+        screenDivs[i].style.display = "block";
+        
+    }
+}
+
+let getOpt = () => {
+    petSel = document.getElementById("newPet").value.toLowerCase();
+}
 
 
 const cat = new Pet('Cat', 'Kitty', 5);
+const rabbit = new Pet('Rabbit', 'Thumper', 7);
+const bird = new Pet('Bird', 'Tweety', 0.001);
 
-let createImg = (animal, i) => {                       
+let createImg = (animal, i) => { 
     console.log(`<img src = "${animal.img[i]}" alt = "Picture of ${animal.name}" />`);
                                             //Should creat a picture of pet.
      img.innerHTML = `<img src = "${animal.img[i]}" alt = "Picture of ${animal.name}" />`;
 }
 
-//createImg(cat, 0);
+// createImg(cat, 0);
 
 // cat.count();
 
@@ -70,4 +106,35 @@ async function revcount(){
 
 
 // revcount();
+
+/* ========================================================================================================================*/
+//User input:
+
+petSel.addEventListener('change', ()=>{
+    input.style.display = 'none';
+    getOpt();
+    console.log(petSel); //cat
+
+    if( petSel == "cat" ) {
+        petSel = cat;
+    } else if ( petSel == "rabbit" ) {
+        petSel = rabbit;
+    } else if ( petSel == "bird" ) {
+        petSel = bird;
+    }
+    
+    createImg(petSel, 0);
+
+    petSel.count();
+
+    gameScreen();
+
+});
+
+
+
+let start = () => {
+
+} 
+
 
